f200ae1428a2d2138ccc7e1b75da4cd58ebc7635
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-11-12 07:55:42 +0530

    small change

--- typeScript/basics/typetest.ts
+++ typeScript/basics/typetest.ts
@@ -6,8 +6,11 @@ const result1 = test1(5,3);
 console.log(result1);
 
 
-let num1: number;
+let num1: number; // setting varaiable to number type
 num1=23;
 
-let arr1: number[];
+let bol1: boolean;
+bol1= true;
+
+let arr1: number[]; //array of numbers
 arr1 = [2,3];

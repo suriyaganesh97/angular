6307288709d6cbd8a22c598817163cafd0c5bdf4
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-11-12 10:10:29 +0530

    secondProject commit

--- typeScript/basics/typetest.js
+++ typeScript/basics/typetest.js
@@ -3,7 +3,17 @@ function test1(a, b) {
 }
 var result1 = test1(5, 3);
 console.log(result1);
-var num1;
+var num1; // setting varaiable to number type
 num1 = 23;
-var arr1;
+var bol1; // boolean type
+bol1 = true;
+var str1; //string
+str1 = 'suriya';
+var arr1; //array of numbers
 arr1 = [2, 3];
+var car;
+car = {
+    name: "toyota",
+    no_of_wheels: 4,
+    is_fast: true
+};
--- typeScript/basics/typetest.ts
+++ typeScript/basics/typetest.ts
@@ -9,8 +9,36 @@ console.log(result1);
 let num1: number; // setting varaiable to number type
 num1=23;
 
-let bol1: boolean;
+let bol1: boolean;  // boolean type
 bol1= true;
+
+let str1: string;  //string
+str1 = 'suriya';
 
 let arr1: number[]; //array of numbers
 arr1 = [2,3];
+
+let car: {  //object car created with 3 attributes of 3 types
+    name: string,
+    no_of_wheels:number,
+    is_fast : boolean;
+}
+
+car = {
+    name : "toyota",
+    no_of_wheels:4,
+    is_fast:true
+}
+
+let name1 : string | number  = 'tester'; //here we can now use string or number for name1 variable
+
+name1=23; //like this
+
+
+//type alias instead of repeating the type again and again we can use an alias like below
+
+type SuperCar ={  //type keyword is used to create a new type and set such alias
+    no_of_engine:number,
+    name:string
+}
+let honda: SuperCar; // we are giving supercar type to the varaible honda

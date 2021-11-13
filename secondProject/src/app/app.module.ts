03015f52f3a96f7f984648ee65026a487ce354f2
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-11-13 19:35:35 +0530

    small assignment

--- secondProject/src/app/app.module.ts
+++ secondProject/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
+import { TestcompComponent } from './testcomp/testcomp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent,
+    TestcompComponent
   ],
   imports: [
     BrowserModule

fe1e1359f2edfe8c0a6630be6b03aa5212df67b4
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-11-21 17:27:03 +0530

    js commit

---
+++ javascript/basics/function.js
@@ -0,0 +1,8 @@
+function test1(){
+    console.log("test1");
+}
+function test2(){
+    return "test2";
+}
+test1();
+console.log(test2());
---
+++ javascript/basics/loops.js
@@ -0,0 +1,11 @@
+for(let i=0;i<4;i++){
+    console.log(i);
+}
+arr1=[20,30,40,50,60];
+for (val of arr1){
+    console.log(val);
+}
+
+for (index in arr1){
+    console.log(index);
+}

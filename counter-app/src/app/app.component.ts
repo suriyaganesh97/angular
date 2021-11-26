ea7184866bb91f96262f12c54dddad2f32145ad8
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-11-26 10:48:08 +0530

    counter app end commit

--- counter-app/src/app/app.component.ts
+++ counter-app/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title = 'Counter App';
+  count = 0;
+  increaseCount(){
+    if(this.count<10)
+    {
+      this.count = this.count + 1;
+    }
+    
+  }
+  decreaseCount(){
+    if(this.count>0){
+      this.count = this.count - 1;
+    }
+    
+  }
+  resetCount(){
+    this.count=0;
+  }
 }

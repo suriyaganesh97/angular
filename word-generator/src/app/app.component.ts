09037603f867a4eb74b342c1df99607545f150e4
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-12-14 16:58:53 +0530

    word gen second commit

--- word-generator/src/app/app.component.ts
+++ word-generator/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import Arraywords from "../utils/words"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+  words = Arraywords;
 }

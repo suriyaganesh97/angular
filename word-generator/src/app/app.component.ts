0f9dee1e118a1aa636430688ad12cd05f3fa1864
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-12-18 13:14:25 +0530

    word gen complete

--- word-generator/src/app/app.component.ts
+++ word-generator/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import Arraywords from "../utils/words"
+import country_names from "../utils/countries"
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,18 +9,23 @@ import Arraywords from "../utils/words"
 })
 export class AppComponent {
   title = 'word-generator';
-  words = Arraywords;
-  word="";
+  
+  words="";
   limit=10;
+  co_data = country_names;
+  random_no=0;
+  random_country='';
 
   slide_func(newLimit :number){
     this.limit = newLimit;
   }
 
-  // new_number=0;
-  
-  // When_clicked(new_number: number)
-  // {
-  //   this.new_number = new_number;
-  // }
+  generate(){
+    this.words = Arraywords.slice(0,this.limit).join(" ");
+  }
+  generate_country(){
+    this.random_no = Math.round(Math.random()*10);
+    this.random_country = this.co_data[this.random_no];
+  }
+
 }
---
+++ word-generator/src/utils/countries.ts
@@ -0,0 +1,3 @@
+export default [
+    'Afghanistan','Albania','Algeria','Andorra','Angola','Antigua','Argentina','Armenia','Australia','Austria','Azerbaijan','Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bhutan'
+]

93a480fcbd031ea5910cfacd9d5eb98559e7b871
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-12-23 20:01:14 +0530

    comp commit

--- tic-tac-toe/src/app/app.component.ts
+++ tic-tac-toe/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+  winMessage: String = '';
+  isCross="false";
+  itemArray:String[] = new Array(9).fill('empty')
+  constructor(private toastr: ToastrService) {}
+  // showSuccess() {
+  //   this.toastr.success('Hello world!', 'Toastr fun!');
+  // }
 }

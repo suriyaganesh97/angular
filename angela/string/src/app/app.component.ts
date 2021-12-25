13f93a340b9a8515d4cce2437c98d250f2b2d212
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-12-25 20:32:29 +0530

    string project first commit

---
+++ angela/string/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'string';
+  fName="";
+  lName="";
+  fullName="";
+  nameInput=false;
+  combineName(){
+    this.fullName = this.fName+this.lName;
+    if (this.fullName.length>0)  //checking whether name is entered 
+    {
+      this.nameInput=true;
+    }
+  }
+}

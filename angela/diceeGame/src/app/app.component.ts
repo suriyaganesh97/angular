8819dd5713dc5d1e92262ae29eb446ba18627aa9
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-12-28 16:55:47 +0530

    dicee completed

--- angela/diceeGame/src/app/app.component.ts
+++ angela/diceeGame/src/app/app.component.ts
@@ -10,26 +10,39 @@ export class AppComponent {
   title = 'diceeGame';
   max_limit= 6;
   min_limit=1;
-  winner="";
+  winner="Play the  game";
+  randomNo1=0;
+  randomNo2=0;
+  img_path_1="";
+  img_src_1="../assets/dice6.png";
+  img_path_2="";
+  img_src_2="../assets/dice6.png";
   
-  randomNo1:number = this.min_limit + Math.floor(Math.random()*(this.max_limit-this.min_limit+1));
-  img_path_1 = "../assets/dice"+this.randomNo1+".png";
-  img_src_1 = this.img_path_1;
-
-  randomNo2:number = this.min_limit + Math.floor(Math.random()*(this.max_limit-this.min_limit+1));
-  img_path_2 = "../assets/dice"+this.randomNo2+".png";
-  img_src_2 = this.img_path_2;
+  generaterandomnos(){
+    this.randomNo1 = this.min_limit + Math.floor(Math.random()*(this.max_limit-this.min_limit+1));
+    this.img_path_1 = "../assets/dice"+this.randomNo1+".png";
+    this.img_src_1 = this.img_path_1;
+  
+    this.randomNo2 = this.min_limit + Math.floor(Math.random()*(this.max_limit-this.min_limit+1));
+    this.img_path_2 = "../assets/dice"+this.randomNo2+".png";
+    this.img_src_2 = this.img_path_2;
+  }
+  
   //img_src = "../assets/dice6.png";
   //to show which winner wins or if draw in top where there is refresh me text
-  if (this.randomNo1 > this.randomNo2)
-  {
-    this.winner="player1";
+  findingWinner(){
+    this.generaterandomnos();
+    if (this.randomNo1 > this.randomNo2)
+    {
+      this.winner="player1 wins";
+    }
+    else if (this.randomNo1 === this.randomNo2)
+    {
+      this.winner="draw";
+    }
+    else{
+      this.winner="player2 wins";
+    }
   }
-  else if (randomNo1 === randomNo2)
-  {
-    winner="draw";
-  }
-  else{
-    winner="player2";
-  }
+  
 }

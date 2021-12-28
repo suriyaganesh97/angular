8d83387309bb2fa27d990f3a0a4537b7b3da79f7
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2021-12-28 10:40:43 +0530

    dicee mid commit

--- angela/diceeGame/src/app/app.component.ts
+++ angela/diceeGame/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'diceeGame';
+  max_limit= 6;
+  min_limit=1;
+  winner="";
+  
+  randomNo1:number = this.min_limit + Math.floor(Math.random()*(this.max_limit-this.min_limit+1));
+  img_path_1 = "../assets/dice"+this.randomNo1+".png";
+  img_src_1 = this.img_path_1;
+
+  randomNo2:number = this.min_limit + Math.floor(Math.random()*(this.max_limit-this.min_limit+1));
+  img_path_2 = "../assets/dice"+this.randomNo2+".png";
+  img_src_2 = this.img_path_2;
+  //img_src = "../assets/dice6.png";
+  //to show which winner wins or if draw in top where there is refresh me text
+  if (this.randomNo1 > this.randomNo2)
+  {
+    this.winner="player1";
+  }
+  else if (randomNo1 === randomNo2)
+  {
+    winner="draw";
+  }
+  else{
+    winner="player2";
+  }
 }

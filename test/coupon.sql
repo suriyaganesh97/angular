1b60296ed682d2f254825e488df87521f9d44fd1
suriyaganesh97 <63084594+suriyaganesh97@users.noreply.github.com>
2022-02-02 12:47:08 +0530

    test1 commit

--- test/coupon.sql
+++
@@ -1,9 +0,0 @@
--- query 1 promotion data
-select  pxp.status,pxp.name as ADMINISTRATIVE_NAME, pxp.priority,pxp.startdate,pxp.enddate,pxauth.comments as Administrative_description
-from px_promotion pxp,px_promoauth pxauth
-where  pxp.px_promotion_id=pxauth.px_promotion_id and pxp.cdrequired = '1' and pxp.enddate>=sysdate;
-
---query 2 coupon data 
-select pxp.name as promotion_name,pxp.startdate,pxp.enddate,pxp.status,pxcd.code as coupon_code
-from px_promotion pxp,px_cdpool pxcd
-where pxp.px_promotion_id=pxcd.reference_id and pxp.cdrequired = '1' and pxp.enddate>=sysdate and pxp.status='1';

c1d303e801519a1513abe8c5891df369c74ab835
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-02-16 20:23:12 +0530

    recipe and shopping list commit

--- projects/recipe-list/src/app/recipeBook/recipe-list/recipe-list.component.ts
+++ projects/recipe-list/src/app/recipeBook/recipe-list/recipe-list.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../../recipeBook/recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [new Recipe('test','test recipe',
+  'https://images.unsplash.com/photo-1640622658799-54e6039d189b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')
+  ,new Recipe('nexttest','second test recipe',
+  'https://images.unsplash.com/photo-1640622658799-54e6039d189b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')]
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
--- projects/recipe-list/src/app/shoppingList/shopping-list/shopping-list.component.ts
+++ projects/recipe-list/src/app/shoppingList/shopping-list/shopping-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [new Ingredient('test ing',12)];
   constructor() { }
 
   ngOnInit(): void {

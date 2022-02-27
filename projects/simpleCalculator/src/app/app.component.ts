8708eef92d333dccfda89494085b91911f7ae290
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-02-27 19:22:20 +0530

    completed simple calculator

--- projects/simpleCalculator/src/app/app.component.ts
+++ projects/simpleCalculator/src/app/app.component.ts
@@ -10,10 +10,31 @@ export class AppComponent {
   num1:number=0;
   num2:number=0;
   num3:number=0;
+  operation:string="";
+  displayResult:boolean=false;
   
   addnum(){
-    this.num3= this.num1 + this.num2;
+    this.num3 = this.num1 + this.num2; 
+    this.operation="addition";
+    this.displayResult=true;
     
-    
+  }
+
+  subnum(){
+    this.num3 = this.num1 - this.num2;
+    this.num3=Math.abs(this.num3);
+    this.operation="subtraction";
+    this.displayResult=true;
+  }
+
+  mulnum(){
+    this.num3 = this.num1 * this.num2;
+    this.operation="multiplication";
+    this.displayResult=true;
+  }
+  divnum(){
+    this.num3 = this.num1 / this.num2;
+    this.operation="division";
+    this.displayResult=true;
   }
 }
